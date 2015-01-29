4209823001ea89e825d5bde828824cd0c5820c9b
Tanmay Rajani <rajani.tanmay@gmail.com>
2015-01-29 19:48:16 +0530

    json working trying diff

--- felicific15/js/script.js
+++ felicific15/js/script.js
@@ -7,7 +7,8 @@ $(window).load(function() { $("#spinner").fadeOut("slow"); });
 
 $(function() {
 	$.getJSON('js/document.json', function(data) {
-	    var template = $('#ce-temp').html();
+		var template = '<center class="titlecenter"><h2>{{technical.ce.first.name}}</h2></center><ul><li>Slogan: {{technical.ce.first.slogan}}</li><li>Date: {{technical.ce.first.date}}</li><li>Description: {{technical.ce.first.description}}</li></ul>';
+	    //var template = $('#ce-temp').html();
 	    var info = Mustache.to_html(template, data);
 	    $('#ce-third').html(info);
 	});
