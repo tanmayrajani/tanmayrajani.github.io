5979ae8410ed58bb8dfb98a59ac64d85a5dfe0af
Tanmay Rajani <rajani.tanmay@gmail.com>
2015-01-29 16:47:35 +0530

    trying mustache}

--- felicific15/js/script.js
+++ felicific15/js/script.js
@@ -1,1 +1,14 @@
 $(window).load(function() { $("#spinner").fadeOut("slow"); });
+// $(document).ready(function(){
+// 	if($('div').hasClass('active')){
+// 		//if($('div'))
+// 	}
+// });
+
+$(function() {
+	$.getJSON('document.json', function(data) {
+	    var template = $('#ce-temp').html();
+	    var info = Mustache.to_html(template, data);
+	    $('#ce-third').html(info);
+	});
+});
