63de4b96d8c8bf72c863e18a05fa91df911a9d16
Tanmay Rajani <rajani.tanmay@gmail.com>
2015-01-30 21:18:55 +0530

    okay

--- felicific15/js/script.js
+++ felicific15/js/script.js
@@ -1,14 +1,11 @@
 $(window).load(function() { $("#spinner").fadeOut("slow"); });
-// $(document).ready(function(){
-// 	if($('div').hasClass('active')){
-// 		//if($('div'))
-// 	}
-// });
 
 $(function() {
 	$.getJSON('js/document.json', function(data) {
+		var branch;
+		var eventNo;
+
 		var template = '<center class="titlecenter"><h2>{{technical.ce.first.name}}</h2></center><ul><li>Slogan: {{technical.ce.first.slogan}}</li><li>Date: {{technical.ce.first.date}}</li><li>Description: {{technical.ce.first.description}}</li></ul>';
-	    //var template = $('#ce-temp').html();
 	    var info = Mustache.to_html(template, data);
 	    $('#ce-third').html(info);
 	});
