be74c63c51dceaa5fd27c2081ded638283475307
Tanmay Rajani <rajani.tanmay@gmail.com>
2015-02-01 11:05:04 +0530

    color change onclick,added images

--- felicific15/js/script.js
+++ felicific15/js/script.js
@@ -1,6 +1,21 @@
 $(window).load(function() { $("#spinner").fadeOut("slow"); });
 
 $(function() {
+	$('.ce-link, .ec-link, .mba-link, .robo-link').click(function() {
+		$('.slide').removeClass('slide-cyan').removeClass('slide-purple').removeClass('slide-d-orange').addClass('slide-blue');
+		$('body').removeClass('body-cyan').removeClass('body-purple').removeClass('body-d-orange').addClass('body-blue');
+	});
+
+	$('.civil-link, .it-link, .bmca-link').click(function() {
+		$('.slide').removeClass('slide-cyan').removeClass('slide-blue').removeClass('slide-d-orange').addClass('slide-purple');
+		$('body').removeClass('body-cyan').removeClass('body-blue').removeClass('body-d-orange').addClass('body-purple');
+	});
+
+	$('.chem-link, .ic-link, .mech-link').click(function() {
+		$('.slide').removeClass('slide-cyan').removeClass('slide-purple').removeClass('slide-blue').addClass('slide-d-orange');
+		$('body').removeClass('body-cyan').removeClass('body-purple').removeClass('body-blue').addClass('body-d-orange');
+	});
+
 	$.getJSON('js/document.json', function(data) {
 		var branch;
 		var eventNo;
