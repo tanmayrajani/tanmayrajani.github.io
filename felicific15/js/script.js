b365f7e6b070804090b93569e1017631ca71632c
Tanmay Rajani <rajani.tanmay@gmail.com>
2015-02-01 17:41:49 +0530

    trying to automate data binding mustache }}

--- felicific15/js/script.js
+++ felicific15/js/script.js
@@ -28,17 +28,29 @@ $(function() {
 
 
 	$.getJSON('js/document.json', function(data) {
-		var branch;
-		var eventNo;
-		var day;
-
+		var branch,actualDay,actualNo;
+		var index,len;
+		var day=['monday', 'tuesday','wednesday','thursday','friday'];
+		var nos=[7,10,11,8,9];
+		var eventNo=['first','second','third','fourth','fifth','sixth','seventh','eighth','nineth','tenth','eleventh'];
+		var str,template2,info2;
 		var template = '<center class="titlecenter"><h2>{{technical.'+branch+'.first.name}}</h2></center><ul><li>Slogan: {{technical.ce.first.slogan}}</li><li>Date: {{technical.ce.first.date}}</li><li>Description: {{technical.ce.first.description}}</li></ul>';
 	    var info = Mustache.to_html(template, data);
 	    $('#ce-third').html(info);
 
-	    var template2 = '<center><h2 class="titlecenter" cellspacing="15px" cellpadding="5px">{{cultural.monday.first.name}}</h2><table class="cult-event"><tr class="trr"><td>Time</td><td>{{cultural.monday.first.time}}</td> </tr><tr class="trr"><td>Place:</td><td>{{cultural.monday.first.place}}</td></tr><tr class="trr"><td>Fees:</td><td>{{cultural.monday.first.fees}}</td></tr><tr class="trr"><td>Co-ordinators:&nbsp;</td><td>{{cultural.monday.first.co-ordinators.c1}}, {{cultural.monday.first.co-ordinators.p1}}<br>{{cultural.monday.first.co-ordinators.c2}}, {{cultural.monday.first.co-ordinators.p2}}</td></tr></table></center><a class="details" href="#monday"><button class="butt3"><< Back</button></a>';
-	    var info2 = Mustache.to_html(template2, data);
-	    $('#monday-second').html(info2);
+	    for (index = 0, len = day.length; index < len; ++index) {
+		    actualDay = day[index];
+		    actualNo = nos[index];
+		    for (var i = 1; i <= actualNo; i++) {
+		   		template2 = '<center><h2 class="titlecenter">{{cultural.'+actualDay+'.'+eventNo[i-1]+'.name}}</h2><table class="cult-event" cellspacing="15px" cellpadding="5px"><tr class="trr"><td>Time</td><td>{{cultural.'+actualDay+'.'+eventNo[i-1]+'.time}}</td> </tr><tr class="trr"><td>Place:</td><td>{{cultural.'+actualDay+'.'+eventNo[i-1]+'.place}}</td></tr><tr class="trr"><td>Fees:</td><td>{{cultural.'+actualDay+'.'+eventNo[i-1]+'.fees}}</td></tr><tr class="trr"><td>Co-ordinators:&nbsp;</td><td>{{cultural.'+actualDay+'.'+eventNo[i-1]+'.co-ordinators.c1}}, {{cultural.'+actualDay+'.'+eventNo[i-1]+'.co-ordinators.p1}}<br>{{cultural.'+actualDay+'.'+eventNo[i-1]+'.co-ordinators.c2}}, {{cultural.'+actualDay+'.'+eventNo[i-1]+'.co-ordinators.p2}}</td></tr></table></center><a class="details" href="#'+actualDay+'"><button class="butt3"><< Back</button></a>';
+	    		info2 = Mustache.to_html(template2, data);
+	    		str=actualDay+'-'+eventNo[i-1];
+	    		$(str).html(info2);
+		   	};
+		}
+	    
+
+	    
 
 
 
