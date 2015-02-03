8677fdf2cdc6e60bcb4aeebf6a2ca93344d40fdc
Tanmay Rajani <rajani.tanmay@gmail.com>
2015-02-03 19:32:30 +0530

    technical events; data-binding added; image replace

--- felicific15/js/script.js
+++ felicific15/js/script.js
@@ -31,16 +31,22 @@ $(function() {
 		var branch;
 		var index,len;
 		var day=['monday', 'tuesday','wednesday','thursday','friday'];
+		var branch=['ce', 'ec','ic','it','civil','mech','chem','mba','bmca','robotics','robo2'];
 		var nos=[7,10,11,8,9];
+		var nos2=[7,10,11,8,9,7,7,7,7,1,1];
 		var eventNo=['first','second','third','fourth','fifth','sixth','seventh','eighth','ninth','tenth','eleventh'];
-		var str,template2,template3,info2;
-		//var template = '<center class="titlecenter"><h2>{{technical.'+branch+'.first.name}}</h2></center><ul><li>Slogan: {{technical.ce.first.slogan}}</li><li>Date: {{technical.ce.first.date}}</li><li>Description: {{technical.ce.first.description}}</li></ul>';
-	    //var info = Mustache.to_html(template, data);
-	    //$('#ce-third').html(info);
+		var str,template2,template,info2,info;
+
+		for (index = 0, len = branch.length; index < len; ++index) {
+			for (var i = 1; i <= nos2[index]; i++) {
+				template = '<center class="titlecenter"><h2>{{technical.'+branch[index]+'.'+eventNo[i-1]+'.name}}</h2></center><center><b class="slogan">{{technical.'+branch[index]+'.'+eventNo[i-1]+'.slogan}}</b></center><table class="event-table"><tr class="trr"><td>Description:&nbsp;</td><td>{{technical.'+branch[index]+'.'+eventNo[i-1]+'.description}}</td> </tr><tr class="trr"><td>Team size:</td><td>{{technical.'+branch[index]+'.'+eventNo[i-1]+'.teamsize}}</td></tr><tr class="trr"><td>Fees:</td><td>{{technical.'+branch[index]+'.'+eventNo[i-1]+'.fees}}</td></tr><tr class="trr"><td>Date-Time:&nbsp;</td><td>{{technical.'+branch[index]+'.'+eventNo[i-1]+'.date}}</td></tr><tr class="trr"><td>Contact:</td><td>{{technical.'+branch[index]+'.'+eventNo[i-1]+'.co-ordinator}}, {{technical.'+branch[index]+'.'+eventNo[i-1]+'.contact}}</td></tr></table><a class="details" href="#'+branch[index]+'"><button class="butt1">Events Home</button></a><a class="details" href="#'+branch[index]+'-'+eventNo[i-1]+'2"><button class="butt">Details</button></a>';
+				info = Mustache.to_html(template, data);
+				str='#'+branch[index]+'-'+eventNo[i-1];
+				$(str).html(info);
+			}
+		}
 
 	    for (index = 0, len = day.length; index < len; ++index) {
-	    	template3='';
-
 		    for (var i = 1; i <= nos[index]; i++) {
 		   		template2 = '<center><h2 class="titlecenter">{{cultural.'+day[index]+'.'+eventNo[i-1]+'.name}}</h2><table class="cult-event" cellspacing="15px" cellpadding="5px"><tr class="trr"><td>Time</td><td>{{cultural.'+day[index]+'.'+eventNo[i-1]+'.time}}</td> </tr><tr class="trr"><td>Place:</td><td>{{cultural.'+day[index]+'.'+eventNo[i-1]+'.place}}</td></tr><tr class="trr"><td>Fees:</td><td>{{cultural.'+day[index]+'.'+eventNo[i-1]+'.fees}}</td></tr><tr class="trr"><td>Co-ordinators:&nbsp;</td><td>{{cultural.'+day[index]+'.'+eventNo[i-1]+'.co-ordinators.c1}}, {{cultural.'+day[index]+'.'+eventNo[i-1]+'.co-ordinators.p1}}<br>{{cultural.'+day[index]+'.'+eventNo[i-1]+'.co-ordinators.c2}}, {{cultural.'+day[index]+'.'+eventNo[i-1]+'.co-ordinators.p2}}</td></tr></table></center><a class="details" href="#'+day[index]+'"><button class="butt3"><< Back</button></a>';
 	    		info2 = Mustache.to_html(template2, data);
