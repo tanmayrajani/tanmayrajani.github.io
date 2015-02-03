915534aa97c2cbb98b581132594ccff0947122e9
Tanmay Rajani <rajani.tanmay@gmail.com>
2015-02-03 19:50:30 +0530

    rules & details; data-binding working awesome

--- felicific15/js/script.js
+++ felicific15/js/script.js
@@ -35,14 +35,18 @@ $(function() {
 		var nos=[7,10,11,8,9];
 		var nos2=[7,10,11,8,9,7,7,7,7,1,1];
 		var eventNo=['first','second','third','fourth','fifth','sixth','seventh','eighth','ninth','tenth','eleventh'];
-		var str,template2,template,info2,info;
+		var str,str2,template2,template,info2,info,template3,info3;
 
 		for (index = 0, len = branch.length; index < len; ++index) {
 			for (var i = 1; i <= nos2[index]; i++) {
 				template = '<center class="titlecenter"><h2>{{technical.'+branch[index]+'.'+eventNo[i-1]+'.name}}</h2></center><center><b class="slogan">{{technical.'+branch[index]+'.'+eventNo[i-1]+'.slogan}}</b></center><table class="event-table"><tr class="trr"><td>Description:&nbsp;</td><td>{{technical.'+branch[index]+'.'+eventNo[i-1]+'.description}}</td> </tr><tr class="trr"><td>Team size:</td><td>{{technical.'+branch[index]+'.'+eventNo[i-1]+'.teamsize}}</td></tr><tr class="trr"><td>Fees:</td><td>{{technical.'+branch[index]+'.'+eventNo[i-1]+'.fees}}</td></tr><tr class="trr"><td>Date-Time:&nbsp;</td><td>{{technical.'+branch[index]+'.'+eventNo[i-1]+'.date}}</td></tr><tr class="trr"><td>Contact:</td><td>{{technical.'+branch[index]+'.'+eventNo[i-1]+'.co-ordinator}}, {{technical.'+branch[index]+'.'+eventNo[i-1]+'.contact}}</td></tr></table><a class="details" href="#'+branch[index]+'"><button class="butt1">Events Home</button></a><a class="details" href="#'+branch[index]+'-'+eventNo[i-1]+'2"><button class="butt">Details</button></a>';
+				template3 = '<center class="titlecenter"><h2>{{technical.'+branch[index]+'.'+eventNo[i-1]+'.name}}</h2></center><table class="event-table"><tr class="trr"><td>Details:&nbsp;</td><td class="det">{{technical.'+branch[index]+'.'+eventNo[i-1]+'.details}}</td> </tr><tr class="trr"><td>Rules:</td><td class="rul">{{technical.'+branch[index]+'.'+eventNo[i-1]+'.rules}}</td></tr></table><a class="details" href="#'+branch[index]+'-'+eventNo[i-1]+'"><button class="butt"><< Back</button></a>';
 				info = Mustache.to_html(template, data);
+				info3 = Mustache.to_html(template3, data);
 				str='#'+branch[index]+'-'+eventNo[i-1];
+				str2=str+'2';
 				$(str).html(info);
+				$(str2).html(info3);
 			}
 		}
 
