1af3a240e34356de4749e1bfd04b3887e9c902ff
Tanmay Rajani <rajani.tanmay@gmail.com>
2015-12-29 12:14:49 +0530

    removed comments and junk

--- ng/main.js
+++ ng/main.js
@@ -6,16 +6,11 @@ myApp.factory('Data',function () {
 myApp.filter('play',function (Data) {
 	return function (text) {
 		text = text.substring(text.lastIndexOf("\n")+1);
-		//console.log(text);
 		var index = text.indexOf(':');
 		if(index>-1){
 			var first = text.substring(0,index);
 			var last = text.substring(index+1);
-			//console.log(first+" : "+last);
 			$(".showtime").css(first,last);
-			// if(text.substring(text.length-1)==';'){
-			//  	$(".ohyes").val($(".ohyes").val()+"\n");
-			// } 
 		}
 		return text;
 	}
